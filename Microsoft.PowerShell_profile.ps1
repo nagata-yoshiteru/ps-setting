2098720d75624ba9b4d06a5a317ca7b66c975225
nagata-yoshiteru <nagata.yoshiteru@g.mbox.nagoya-u.ac.jp>
2020-12-31 18:50:30 +0900

    change time format

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,5 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 Import-Module posh-git
-$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
+$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM/dd HH:mm:ss") '
 $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Cyan

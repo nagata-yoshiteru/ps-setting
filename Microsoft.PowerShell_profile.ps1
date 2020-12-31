2b1e9f2cd8f8a209eb4934d23a5fe650f2e26ff5
nagata-yoshiteru <nagata.yoshiteru@g.mbox.nagoya-u.ac.jp>
2020-12-31 18:45:17 +0900

    add posh-git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,6 @@
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
--- README.md
+++ README.md
@@ -3,3 +3,9 @@ My PowerShell Setting ( for v7.1 )
 
 ## Usage
 Put this repository into `C:\Users\username\Documents\PowerShell`.
+
+## Prerequisites
+
+```
+Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+```
